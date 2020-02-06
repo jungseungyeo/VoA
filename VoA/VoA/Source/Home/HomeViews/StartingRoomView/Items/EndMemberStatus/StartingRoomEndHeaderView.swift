2fcb5315f85851934f4511dd05d67fc2bd81d8e3
linsaeng <duwjdtmd91@gmail.com>
2020-02-06 18:40:12 +0900

    making statingMemeber

---
+++ VoA/VoA/Source/Home/HomeViews/StartingRoomView/Items/EndMemberStatus/StartingRoomEndHeaderView.swift
@@ -0,0 +1,46 @@
+//
+//  StartingRoomEndHeaderView.swift
+//  VoA
+//
+//  Created by saeng lin on 2020/02/05.
+//  Copyright © 2020 Linsaeng. All rights reserved.
+//
+
+import UIKit
+
+class StartingRoomEndHeaderView: BaseCollectionReusableView {
+    
+    static let registerID: String = "\(StartingRoomEndHeaderView.self)"
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.attributedText = Const.titleString
+        return label
+    }()
+    
+    private struct Const {
+        static let titleString: NSAttributedString = .init(string: "귀가한 맴버",
+                                                           font: .systemFont(ofSize: 14,
+                                                                             weight: .bold),
+                                                           color: VoAColor.StartingRoom.noneStartTitleColor)
+    }
+    
+    override func setup() {
+        super.setup()
+        
+        addSubviews(titleLabel)
+    }
+    
+    override func setupUI() {
+        super.setupUI()
+        
+        titleLabel.snp.remakeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.left.equalToSuperview().offset(16)
+        }
+    }
+    
+    func bind(memberCount: Int?) {
+        titleLabel.text = "귀가한 맴버 \(memberCount ?? 0)"
+    }
+}
